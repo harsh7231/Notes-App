caae9dac4fbea07d439279431a797a9af4d139a9
Harsh Kumar rattewal <91414873+harsh7231@users.noreply.github.com>
2023-09-11 21:55:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,34 +4,34 @@ import Laptop from "./pages/Laptop";
 import Phone from "./pages/Phone";
 import MNotes from "./components/Notes/MNotes";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Provider} from "./useContext";
+import { Provider } from "./useContext";
 import UseContext from "./useContext";
 
 function App() {
-  const [screenSize, setScreenSize] = useState(window.innerWidth);
   const { selected, setSelected } = UseContext();
 
   useEffect(() => {
     setSelected(localStorage.getItem("selected") || "");
     // eslint-disable-next-line
   }, [selected]);
-  const checkScreenSize = () => {
-    setScreenSize(window.innerWidth);
+
+  const isMobile = () => {
+    // Use media query to check if the device is mobile
+    return window.matchMedia("(max-width: 500px)").matches;
   };
 
-  window.addEventListener("resize", checkScreenSize);
   return (
     <Provider>
       <div className="App">
-        {screenSize > 500 ? (
-          <Laptop />
-        ) : (
+        {isMobile() ? (
           <Router>
             <Routes>
               <Route path="/" element={<Phone />} />
               <Route path="/notes" element={<MNotes />} />
             </Routes>
           </Router>
+        ) : (
+          <Laptop />
         )}
       </div>
     </Provider>
